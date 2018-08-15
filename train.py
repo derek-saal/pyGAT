dc476204c9bd0e1bd4ad5ea7b8e67f21398459da
Dane <danesherbs@gmail.com>
2018-08-15 17:32:04 +0100

    Fixed hardcoded GAT

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ parser.add_argument('--dropout', type=float, default=0.6, help='Dropout rate (1 
 parser.add_argument('--alpha', type=float, default=0.2, help='Alpha for the leaky_relu.')
 parser.add_argument('--patience', type=int, default=100, help='Patience')
 
-args = parser.parse_args(['--model', 'GCN'])
+args = parser.parse_args()
 
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
