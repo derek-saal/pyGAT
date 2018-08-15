ad04f0e64fb524a891bc4dbb7b4e96f78ead0da2
Dane <danesherbs@gmail.com>
2018-08-15 14:55:40 +0100

    Added remove thingo

--- train.py
+++ train.py
@@ -133,11 +133,11 @@ for epoch in range(args.epochs):
         if epoch_nb < best_epoch:
             os.remove(file)
 
-# files = glob.glob('*.pkl')
-# for file in files:
-#     epoch_nb = int(file.split('.')[0])
-#     if epoch_nb > best_epoch:
-#         os.remove(file)
+files = glob.glob('*.pkl')
+for file in files:
+    epoch_nb = int(file.split('.')[0])
+    if epoch_nb > best_epoch:
+        os.remove(file)
 
 print("Optimization Finished!")
 print("Total time elapsed: {:.4f}s".format(time.time() - t_total))
