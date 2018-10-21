3065984141a79f3c6a0c0be59cadf7c42769efac
derek-saal <derek@saal.ai>
2018-10-21 12:42:29 +0400

    Updates for matching build graph from text_gcn. Very close

--- src/data_proc/build_graph.py
+++ src/data_proc/build_graph.py
@@ -16,256 +16,83 @@ def build_graph(dataset):
     word_embeddings_dim = 300
     word_vector_map = {}
 
-    # shuffling
-    corpus_name_list = []
-    corpus_train_list = []
-    corpus_test_list = []
-
     df_info = pd.read_csv(data_dir/'ids_and_labels.txt', sep='\t', header=None, names=['id', 'test_train', 'label'])
     df_data = pd.read_csv(data_dir/'clean_filtered.txt', sep='\t', header=None, names=['text'])
     df = pd.concat([df_info, df_data], axis=1)
     df.text = df.text.apply(str)
 
-
-    # build vocab
-    word_freq = defaultdict(int)
-    module_logger.info(f"Building vocab")
-
-    def get_words(cell):
-        words = cell.split()
-        for word in words:
-            word_freq[word] += 1
-    df.text.apply(get_words)
-    word_freq = dict(word_freq)
-
-    vocab = list(word_freq.keys())
-    vocab_size = len(vocab)
-
-    word_doc_list = {}
-
-    for i, shuffle_doc_word in enumerate(shuffle_doc_words_list):
-        words = shuffle_doc_word.split()
-        appeared = set()
-        for word in words:
-            if word in appeared:
-                continue
-            if word in word_doc_list:
-                doc_list = word_doc_list[word]
-                doc_list.append(i)
-                word_doc_list[word] = doc_list
-            else:
-                word_doc_list[word] = [i]
-            appeared.add(word)
-
-    # word_doc_freq = {}
-    # for word, doc_list in word_doc_list.items():
-    #     word_doc_freq[word] = len(doc_list)
-
-    word_id_map = {}
-    for i,vocab_word in enumerate(vocab):
-        word_id_map[vocab_word] = i
-
-    # vocab_str = '\n'.join(vocab)
-    #
-    # with open(data_dir/'vocab.txt', 'w') as f:
-    #     module_logger.info(f"Writing vocab to: {f.name}")
-    #     f.write(vocab_str)
-
-    # x: feature vectors of training docs, no initial features
-    # slect 90% training set
-    train_size = len(train_ids)
-    val_size = int(0.1 * train_size)
-    real_train_size = train_size - val_size  # - int(0.5 * train_size)
-    # different training rates
-
-    real_train_doc_names = shuffle_doc_name_list[:real_train_size]
-    real_train_doc_names_str = '\n'.join(real_train_doc_names)
-
-    with open(data_dir/'real_train_name.txt', 'w') as f:
-        f.write(real_train_doc_names_str)
-
-    row_x = []
-    col_x = []
-    data_x = []
-    y = []
-    for i in range(real_train_size):
-        # Calculating x
-        doc_vec = np.zeros(word_embeddings_dim)
-        doc_words = shuffle_doc_words_list[i]
-        words = doc_words.split()
-        doc_len = len(words)
-        if not doc_len:
-            continue
-
-        assert doc_len > 0, "Doc_len is 0"
-        for word in words:
-            if word in word_vector_map:
-                word_vector = word_vector_map[word]
-                doc_vec = doc_vec + np.array(word_vector)
-
-        for j in range(word_embeddings_dim):
-            row_x.append(i)
-            col_x.append(j)
-            data_x.append(doc_vec[j] / doc_len)  # doc_vec[j]/ doc_len
-
-        # Calculating y
-        doc_meta = shuffle_doc_name_list[i]
-        temp = doc_meta.split('\t')
-        label = temp[2]
-        one_hot = [0 for l in range(len(label_list))]
-        label_index = label_list.index(label)
-        one_hot[label_index] = 1
-        y.append(one_hot)
-
-    # x = sp.csr_matrix((real_train_size, word_embeddings_dim), dtype=np.float32)
-    x = sp.csr_matrix((data_x, (row_x, col_x)), shape=(
-        real_train_size, word_embeddings_dim))
-
-    y = np.array(y)
-
-    # tx: feature vectors of test docs, no initial features
-    test_size = len(test_ids)
-
-    row_tx = []
-    col_tx = []
-    data_tx = []
-    ty = []
-    for i in range(test_size):
-        # Calculating tx
-        doc_vec = np.array([0.0 for k in range(word_embeddings_dim)])
-        doc_words = shuffle_doc_words_list[i + train_size]
-        words = doc_words.split()
-        doc_len = len(words)
-        if not doc_len:
-            continue
-
-        for word in words:
-            if word in word_vector_map:
-                word_vector = word_vector_map[word]
-                doc_vec = doc_vec + np.array(word_vector)
-
-        for j in range(word_embeddings_dim):
-            row_tx.append(i)
-            col_tx.append(j)
-            # np.random.uniform(-0.25, 0.25)
-            data_tx.append(doc_vec[j] / doc_len)  # doc_vec[j] / doc_len
-
-        # Calculating ty
-        doc_meta = shuffle_doc_name_list[i + train_size]
-        temp = doc_meta.split('\t')
-        label = temp[2]
-        one_hot = [0 for _ in range(len(label_list))]
-        label_index = label_list.index(label)
-        one_hot[label_index] = 1
-        ty.append(one_hot)
-
-    # tx = sp.csr_matrix((test_size, word_embeddings_dim), dtype=np.float32)
-    tx = sp.csr_matrix((data_tx, (row_tx, col_tx)),
-                       shape=(test_size, word_embeddings_dim))
-
-    ty = np.array(ty)
-
-    # allx: the the feature vectors of both labeled and unlabeled training instances
-    # (a superset of x)
-    # unlabeled training instances -> words
-
-    word_vectors = np.random.uniform(-0.01, 0.01,
-                                     (vocab_size, word_embeddings_dim))
-
-    for i in range(len(vocab)):
-        word = vocab[i]
-        if word in word_vector_map:
-            vector = word_vector_map[word]
-            word_vectors[i] = vector
-
-    row_allx = []
-    col_allx = []
-    data_allx = []
-    ally = []
-
-    for i in range(train_size):
-        doc_vec = np.array([0.0 for k in range(word_embeddings_dim)])
-        doc_words = shuffle_doc_words_list[i]
-        words = doc_words.split()
-        doc_len = len(words)
-        if doc_len == 0:
-            continue
-        assert doc_len > 0, "Doc_len is 0"
-        for word in words:
-            if word in word_vector_map:
-                word_vector = word_vector_map[word]
-                doc_vec = doc_vec + np.array(word_vector)
-
-        for j in range(word_embeddings_dim):
-            row_allx.append(int(i))
-            col_allx.append(j)
-            # np.random.uniform(-0.25, 0.25)
-            data_allx.append(doc_vec[j] / doc_len)  # doc_vec[j]/doc_len
-
-        doc_meta = shuffle_doc_name_list[i]
-        temp = doc_meta.split('\t')
-        label = temp[2]
-        one_hot = [0 for _ in range(len(label_list))]
-        label_index = label_list.index(label)
-        one_hot[label_index] = 1
-        ally.append(one_hot)
-    for i in range(vocab_size):
-        for j in range(word_embeddings_dim):
-            row_allx.append(int(i + train_size))
-            col_allx.append(j)
-            data_allx.append(word_vectors.item((i, j)))
-
-
-    row_allx = np.array(row_allx)
-    col_allx = np.array(col_allx)
-    data_allx = np.array(data_allx)
-
-    allx = sp.csr_matrix(
-        (data_allx, (row_allx, col_allx)), shape=(train_size + vocab_size, word_embeddings_dim))
-
-
-    for i in range(vocab_size):
-        one_hot = [0 for l in range(len(label_list))]
-        ally.append(one_hot)
-
-    ally = np.array(ally)
-
-    module_logger.info(f"\nx: {x.shape}\ty: {y.shape}\ntx: {tx.shape}\tty: {ty.shape}\nallx: {allx.shape}\tally: {ally.shape}")
-
-    '''
-    Doc word heterogeneous graph
-    '''
-
-    # word co-occurence with context windows
-    window_size = 20
-    windows = []
-
-    for doc_words in shuffle_doc_words_list:
-        words = doc_words.split()
+    def build_windows(text, window_size=20):
+        windows = []
+        words = text.split()
         length = len(words)
         if length <= window_size:
             windows.append(words)
         else:
-            # print(length, length - window_size + 1)
             for j in range(length - window_size + 1):
                 window = words[j: j + window_size]
                 windows.append(window)
-                # print(window)
+        return windows
 
+    t = df.text.apply(build_windows)
+    windows = [sublist for l in t for sublist in l]
 
     word_window_freq = {}
+    # Find number of non-repeated words in windows
     for window in windows:
         appeared = set()
-        for i in range(len(window)):
-            if window[i] in appeared:
+        for window_word in window:
+            # skip if we have seen it before in window
+            if window_word in appeared:
                 continue
-            if window[i] in word_window_freq:
-                word_window_freq[window[i]] += 1
+            if window_word in word_window_freq:
+                word_window_freq[window_word] += 1
             else:
-                word_window_freq[window[i]] = 1
-            appeared.add(window[i])
+                word_window_freq[window_word] = 1
+            appeared.add(window_word)
+
+    vocab = set()
+    vocab_size = len(vocab)
+    module_logger.info(f"Building Vocab...")
+    for window in windows:
+        for window_word in window:
+            vocab.add(window_word)
+    vocab = list(vocab)
+    module_logger.info(f"\tVocab length: {len(vocab)}")
+
+
+    def create_word_doc_list(series):
+        """
+        Create word_doc_list
+        :param series:
+        :return:
+        """
+        word_doc_list = {}
+        for word_id, doc_words in enumerate(series):
+            words = doc_words.split()
+            appeared = set()
+            for word in words:
+                if word in appeared:
+                    continue
+                if word in word_doc_list:
+                    doc_list = word_doc_list[word]
+                    doc_list.append(word_id)
+                    word_doc_list[word] = doc_list
+                else:
+                    word_doc_list[word] = [word_id]
+                appeared.add(word)
+        return word_doc_list
+    word_doc_list = create_word_doc_list(df.text)
+    word_doc_freq = {}
+
+    for word, doc_list in word_doc_list.items():
+        word_doc_freq[word] = len(doc_list)
+
+    word_id_map = {}
+    for i, word in enumerate(vocab):
+        word_id_map[word] = i
 
     word_pair_count = {}
+    module_logger.info(f"Building word_pair counts...")
     for window in windows:
         for i in range(1, len(window)):
             for j in range(0, i):
@@ -286,15 +113,15 @@ def build_graph(dataset):
                     word_pair_count[word_pair_str] += 1
                 else:
                     word_pair_count[word_pair_str] = 1
+    module_logger.info(f"\Word Pairs Count: {len(word_pair_count)}")
 
+    # pmi as weights
     row = []
     col = []
     weight = []
-
-    # pmi as weights
-
     num_window = len(windows)
 
+    module_logger(f"Calculating PMI...")
     for key in word_pair_count:
         temp = key.split(',')
         i = int(temp[0])
@@ -306,30 +133,17 @@ def build_graph(dataset):
                   (1.0 * word_freq_i * word_freq_j/(num_window * num_window)))
         if pmi <= 0:
             continue
-        row.append(train_size + i)
-        col.append(train_size + j)
+        # ToDo figure out how this works
+        # weights.append()
+        # row.append(train_size + i)
+        # col.append(train_size + j)
         weight.append(pmi)
 
-    # word vector cosine similarity as weights
 
-    '''
-    for i in range(vocab_size):
-        for j in range(vocab_size):
-            if vocab[i] in word_vector_map and vocab[j] in word_vector_map:
-                vector_i = np.array(word_vector_map[vocab[i]])
-                vector_j = np.array(word_vector_map[vocab[j]])
-                similarity = 1.0 - cosine(vector_i, vector_j)
-                if similarity > 0.9:
-                    print(vocab[i], vocab[j], similarity)
-                    row.append(train_size + i)
-                    col.append(train_size + j)
-                    weight.append(similarity)
-    '''
     # doc word frequency
     doc_word_freq = {}
 
-    for doc_id in range(len(shuffle_doc_words_list)):
-        doc_words = shuffle_doc_words_list[doc_id]
+    for doc_id, doc_words in enumerate(df.text):
         words = doc_words.split()
         for word in words:
             word_id = word_id_map[word]
@@ -339,22 +153,21 @@ def build_graph(dataset):
             else:
                 doc_word_freq[doc_word_str] = 1
 
-    for i in range(len(shuffle_doc_words_list)):
-        doc_words = shuffle_doc_words_list[i]
+    for doc_id, doc_words in enumerate(df.text):
         words = doc_words.split()
         doc_word_set = set()
         for word in words:
             if word in doc_word_set:
                 continue
             j = word_id_map[word]
-            key = str(i) + ',' + str(j)
+            key = str(doc_id) + ',' + str(j)
             freq = doc_word_freq[key]
-            if i < train_size:
-                row.append(i)
+            if doc_id < train_size:
+                row.append(doc_id)
             else:
-                row.append(i + vocab_size)
+                row.append(doc_id + vocab_size)
             col.append(train_size + j)
-            idf = log(1.0 * len(shuffle_doc_words_list) /
+            idf = log(1.0 * len(df.text) /
                       word_doc_freq[vocab[j]])
             weight.append(freq * idf)
             doc_word_set.add(word)
@@ -362,35 +175,6 @@ def build_graph(dataset):
     node_size = train_size + vocab_size + test_size
     adj = sp.csr_matrix(
         (weight, (row, col)), shape=(node_size, node_size))
-
-    # dump objects
-    f = open(data_dir/"ind_x.pkl".format(dataset), 'wb')
-    pkl.dump(x, f)
-    f.close()
-
-    f = open(data_dir/"ind_y.pkl".format(dataset), 'wb')
-    pkl.dump(y, f)
-    f.close()
-
-    f = open(data_dir/"ind_tx.pkl".format(dataset), 'wb')
-    pkl.dump(tx, f)
-    f.close()
-
-    f = open(data_dir/"ind_ty.pkl".format(dataset), 'wb')
-    pkl.dump(ty, f)
-    f.close()
-
-    f = open(data_dir/"ind_allx.pkl".format(dataset), 'wb')
-    pkl.dump(allx, f)
-    f.close()
-
-    f = open(data_dir/"ind_ally.pkl".format(dataset), 'wb')
-    pkl.dump(ally, f)
-    f.close()
-
-    f = open(data_dir/"ind_adj.pkl".format(dataset), 'wb')
-    pkl.dump(adj, f)
-    f.close()
 
 
 if __name__ == '__main__':
